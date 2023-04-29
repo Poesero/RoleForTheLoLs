68d5870df134f4435eeb40c5f456a57c85e32a27
Poesero <81367033+Poesero@users.noreply.github.com>
2023-04-29 16:22:22 -0300

    sabado29

--- src/main/java/repository/PostRepository.java
+++ src/main/java/repository/PostRepository.java
@@ -1,4 +1,13 @@
 package repository;
 
-public class PostRepository {
+import model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post,Integer> {
+
+    @Query(value =" select usuario_id from usuario_post where post_id = : postId",nativeQuery = true)
+    List<Integer> findUsuarioByPostid(Integer postId);
 }
--- src/main/java/repository/UsuarioRepository.java
+++ src/main/java/repository/UsuarioRepository.java
@@ -1,4 +1,11 @@
 package repository;
 
-public class UsuarioRepository {
+import model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
+
+
 }
