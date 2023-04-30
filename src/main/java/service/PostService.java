f3df55a895895b00166b225feca7de5a84fe2e24
Poesero <81367033+Poesero@users.noreply.github.com>
2023-04-29 22:10:27 -0300

    sabado29noche

--- src/main/java/service/PostService.java
+++ src/main/java/service/PostService.java
@@ -1,4 +1,44 @@
 package service;
 
+import model.Usuario;
+import model.Post;
+import model.UsuarioDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.modelmapper.ModelMapper;
+import repository.PostRepository;
+import repository.UsuarioRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
 public class PostService {
+
+    private final PostRepository pr;
+
+    private final UsuarioRepository ur;
+
+    private final ModelMapper mm = new ModelMapper();
+
+    @Autowired
+    public PostService(PostRepository pr, UsuarioRepository ur) { this.pr = pr;
+        this.ur = ur;}
+
+    public ResponseEntity addPost(Post p){
+        pr.save(p);
+        return ResponseEntity.status(CREATED).build();
+    }
+
+    public List<UsuarioDTO> findUsuarioByPostId(Integer id)
+    {
+        List<Integer> usuarioIdList = pr.findUsuarioByPostid(id);
+        List<UsuarioDTO> usuarios = new ArrayList<>();
+        for (Integer usuarioId : usuarioIdList) {
+            Usuario u = ur.findById(usuarioId).orElseThrow();
+            usuarios.add(mm.map(u, UsuarioDTO.class));
+        }
+    return usuarios;
+    }
 }
--- src/main/java/service/UsuarioService.java
+++ src/main/java/service/UsuarioService.java
@@ -1,4 +1,34 @@
 package service;
 
+import model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import repository.UsuarioRepository;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@Service
 public class UsuarioService {
+
+    private final UsuarioRepository ur;
+
+    @Autowired
+    public ServicioUsuario(UsuarioRepository ur){this.ur = ur;}
+
+    public ResponseEntity addUsuario(Usuario u){
+        ur.save(u);
+        return ResponseEntity.status(CREATED).build();
+    }
+
+    public List<Usuario> getAll(){return ur.findAll();}
+
+    public ResponseEntity updateUsuario(Integer id, Usuario u) {
+        Usuario ut = ur.findById(id).orElseThrow(() -> new HttpClientErrorException(HttpStatus.BAD_REQUEST, "usuario no encontrado"));
+        ut.setUserName(u.getUserName());
+    }
 }
